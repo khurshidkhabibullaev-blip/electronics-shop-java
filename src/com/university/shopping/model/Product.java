c5981e2262b9df1140616fea7c7718ee687f9ef4
khurshidkhabibullaev-blip <khurshidkhabibullaev@gmail.com>
2026-02-01 21:13:16 +0500

    Product.java

--- src/com/university/shopping/model/Product.java
+++ src/com/university/shopping/model/Product.java
@@ -1,4 +1,28 @@
 package com.university.shopping.model;
 
 public class Product {
+   public int id;
+    public String name;
+    public double price;
+    public String category;
+
+    public Product (int id, String name, double price, String category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId(){return id;}
+    public String getName(){return name;}
+    public double getPrice(){return price;}
+    public String getCategory(){return category;}
+
+    public String toString(){
+        return "Product: " + name + ", Price: " + price + " USD";
+    }
+
+
+
+
 }
