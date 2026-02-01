2945ad9daac6ec0b293e521316e29c6c10d5971e
khurshidkhabibullaev-blip <khurshidkhabibullaev@gmail.com>
2026-02-01 21:33:22 +0500

    OrderItem.java

--- src/com/university/shopping/model/OrderItem.java
+++ src/com/university/shopping/model/OrderItem.java
@@ -1,4 +1,22 @@
 package com.university.shopping.model;
 
 public class OrderItem {
+
+    public Product product;
+    public int quantity;
+
+    public OrderItem(Product product, int quantity){
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice(){
+        return product.price * quantity;
+    }
+
+    public String toString(){
+        return product.name + "x" + quantity + "=" + getTotalPrice();
+    }
+
 }
+
