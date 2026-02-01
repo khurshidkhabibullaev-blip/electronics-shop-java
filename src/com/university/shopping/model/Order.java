2ec5d12b3c0cd6a739ea4bdace35989d50621b2e
khurshidkhabibullaev-blip <khurshidkhabibullaev@gmail.com>
2026-02-01 21:24:23 +0500

    Order.java

--- src/com/university/shopping/model/Order.java
+++ src/com/university/shopping/model/Order.java
@@ -1,4 +1,22 @@
 package com.university.shopping.model;
 
 public class Order {
+   public int orderId;
+    public User user;
+    public Cart cart;
+    public double totalAmount;
+
+    public Order(int orderId, User user, Cart cart){
+        this.orderId = orderId;
+        this.user = user;
+        this.cart = cart;
+        this.totalAmount = cart.getTotalPrice();
+    }
+
+    public void printInvoice(){
+        System.out.println("Bill №" + orderId + ";");
+        System.out.println("Customer: " + user.username + ";");
+        System.out.println("Total price to pay: " + totalAmount + " $");
+        System.out.println("===========================");
+    }
 }
