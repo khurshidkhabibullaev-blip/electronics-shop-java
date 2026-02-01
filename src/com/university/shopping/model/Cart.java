de52f3a226d01a5d22696e151717f3be8f32ac28
khurshidkhabibullaev-blip <khurshidkhabibullaev@gmail.com>
2026-02-01 21:31:21 +0500

    Cart.java

--- src/com/university/shopping/model/Cart.java
+++ src/com/university/shopping/model/Cart.java
@@ -1,4 +1,23 @@
 package com.university.shopping.model;
 
+import java.util.ArrayList;
+
 public class Cart {
+
+public ArrayList<OrderItem> items = new ArrayList<>();
+
+public void addItem(Product product, int quantity){
+    OrderItem newItem = new OrderItem(product, quantity);
+    this.items.add(newItem);
 }
+
+public double getTotalPrice(){
+    double sum = 0;
+    for(OrderItem item : items){
+        sum = sum + (item.product.price * item.quantity);
+    }
+    return sum;
+}
+
+}
+
