a06068835e9fb361f7f20ee5fcaa4a8065107235
khurshidkhabibullaev-blip <khurshidkhabibullaev@gmail.com>
2026-02-01 21:18:05 +0500

    User.java

--- src/com/university/shopping/model/User.java
+++ src/com/university/shopping/model/User.java
@@ -1,4 +1,19 @@
 package com.university.shopping.model;
 
 public class User {
+  public int id;
+    public String username;
+    public String password;
+    public String email;
+
+
+    public User(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    public String toString(){
+        return "User: " + username + ", Password: " + password + ", Email: " + email;
+    }
 }
